88dbd6d9427e1b8dd3f1aae41fca5db8891afe07
Pushpendra <102226260+Pushpendra86@users.noreply.github.com>
2025-04-13 19:26:07 +0530

    init commit

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello this is first file");
+        System.out.println("hello this is second file");
     }
 }
